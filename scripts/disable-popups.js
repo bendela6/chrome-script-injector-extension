df18db5809a8f2050d6d0da0f8ada0d901d3efce
Beka Bendeliani <beka.bendeliani@toptal.com>
2025-10-21 05:52:38 +0400

    Enhance popup blocking by overriding createElement and disabling click events on links

--- scripts/disable-popups.js
+++ scripts/disable-popups.js
@@ -1,17 +1,16 @@
-// 1. Store the original document.createElement
 const originalCreateElement = document.createElement;
 
-const blackList = ["iframe", "script", "link", "meta", "a"];
-
-// 2. Override the global document.createElement function
+// override createElement to block certain tags
 document.createElement = function (tagName) {
-  if (!blackList.includes(tagName.toLowerCase())) {
+  const blackListElements = ["iframe", "script", "link", "meta", "a"];
+  const blackListAttributes = ["href", "src", "target"];
+  if (!blackListElements.includes(tagName.toLowerCase())) {
     return originalCreateElement.apply(document, arguments);
   }
 
   const element = originalCreateElement.apply(document, arguments);
   element.style.display = "none";
-  ["href", "src", "target"].forEach((attr) => {
+  blackListAttributes.forEach((attr) => {
     Object.defineProperty(element, attr, {
       get: function () {
         return "";
@@ -24,6 +23,35 @@ document.createElement = function (tagName) {
   return element;
 };
 
+// disable click popups
+document.addEventListener(
+  "click",
+  function (event) {
+    let target = event.target;
+    while (target && target !== document) {
+      if (target?.tagName?.toLowerCase() === "a") {
+        const href = target?.getAttribute("href");
+        const targetAttr = target?.getAttribute("target");
+        if (href && targetAttr) {
+          event.preventDefault();
+          console.log(`Blocked popup link to ${href}`);
+          return;
+        }
+      }
+      target = target?.parentNode;
+    }
+  },
+  true
+);
+
+// disable window.open
 window.open = function () {
   console.log("Disable window.open");
 };
+
+// const div = document.createElement("div");
+// div.innerHTML =
+//   "<a href='https://youtube.com' target='_blank' style='position: fixed; z-index: 1000; top:0; left:0; background:red'>youtube.com</a>";
+// document.body.appendChild(div);
+// const a = div.querySelector("a");
+// a.click();
